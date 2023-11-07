f8a1439b7cd0c47e8e08127db564eade8a8386aa
mikkim14 <kimkimcanaria14@gmail.com>
2023-11-07 13:15:48 +0800

    feat add coulumn price query

--- hero.sql
+++ hero.sql
@@ -154,3 +154,7 @@ VALUES
     (9, 9, 109),  -- Leomord has Blade Armor
     (10, 10, 110);  -- Chou has Feather of Heaven
 
+/*3.	Create new branch named "feat/add-column-price"*/
+-- add column price
+ALTER TABLE public.item
+ADD COLUMN item_price DECIMAL(10, 2);
